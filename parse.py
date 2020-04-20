091c6aad841302023ccac1f270af191f6d45d9ee
seongha <doubleb@kaist.ac.kr>
2020-04-21 02:33:21 +0900

    track, make Video function as class method & example file

--- parse.py
+++ parse.py
@@ -1,10 +1,45 @@
 import json
 
 class Video():
-    def __init__(self, frames, behaviors, hum_cnt):
-        self.frames = frames #[Frame]
-        self.behaviors = behaviors #[Behavior]
-        self.hum_cnt = hum_cnt #number of humans
+    # def __init__(self, frame, behavior, hmcnt):
+    #     frame = frame 
+    #     behavior = behavior 
+    #     hmcnt = hmcnt
+
+    def __init__(self, name):
+        with open(name) as json_file:
+            json_string = json.load(json_file)
+        json_data = json.loads(json_string)
+        hum_cnt = 0
+
+        for i in range(len(json_data)):
+            boxes = json_data[str(i)]
+            # if (len(boxes) == 0):
+            #     continue
+            humans = []
+            for box_ind in boxes:
+                each = Human(boxes[box_ind]['id'], i, boxes[box_ind]['pose_pos'])
+                # print(boxes[box_ind]['pose_pos'])
+                humans.append(each)
+                # print(each)
+            # print(frame.__dict__)
+            # frames.append(Frame(i, humans))
+            Frame(i, humans)
+            # print(len(humans))
+            if (hum_cnt < len(boxes)):
+                hum_cnt = len(boxes)
+        # return Video(Frame, Behavior, hum_cnt)
+        self.frame = Frame
+        self.behavior = Behavior
+        self.hmcnt = hum_cnt
+
+    def Track(self, bh, start, end):
+        trk = []
+        for frame_id in range(start, end):
+            for human in Frame.get(frame_id).humans:
+                li = [ human.pose_pos[anker] for anker in bh.akpnt]
+                trk.append(json.dumps({'frame_id': human.frame_id, 'human_id': human.id, 'pose_pos': li}, sort_keys=True))
+        return trk
 
 class Behavior():
     instances = []
@@ -38,14 +73,46 @@ class Frame():
         for inst in cls.instances:
             if (inst.id == id):
                 return inst
+    @classmethod
+    def getLength(cls):
+        return len(cls.instances)
+    
+
+
+# def makeVideo(name):
+#     with open(name) as json_file:
+#         json_string = json.load(json_file)
+#     json_data = json.loads(json_string)
+#     hum_cnt = 0
+
+#     for i in range(len(json_data)):
+#         boxes = json_data[str(i)]
+#         # if (len(boxes) == 0):
+#         #     continue
+#         humans = []
+#         for box_ind in boxes:
+#             each = Human(boxes[box_ind]['id'], i, boxes[box_ind]['pose_pos'])
+#             # print(boxes[box_ind]['pose_pos'])
+#             humans.append(each)
+#             # print(each)
+#         # print(frame.__dict__)
+#         # frames.append(Frame(i, humans))
+#         Frame(i, humans)
+#         # print(len(humans))
+#         if (hum_cnt < len(boxes)):
+#             hum_cnt = len(boxes)
+#     return Video(Frame, Behavior, hum_cnt)
+
+# vd = makeVideo('../(G)I-DLE-01_matching.json')
+# print (vd.frame.get(10).__dict__)
 
 
 
-print("generating json deserialization... ")
-with open('../(G)I-DLE-01_matching.json') as json_file:
-    json_string = json.load(json_file)
+# print("generating json deserialization... ")
+# with open('../(G)I-DLE-01_matching.json') as json_file:
+#     json_string = json.load(json_file)
 
-json_data = json.loads(json_string)
+# json_data = json.loads(json_string)
 
 # print("frame index ranges  from 0 to ", len(json_data))
 # frame_ind = input("enter frame index: ")
@@ -65,21 +132,21 @@ frames = []
 behaviors = []
 hum_cnt = 0
 
-for i in range(len(json_data)):
-    boxes = json_data[str(i)]
-    # if (len(boxes) == 0):
-    #     continue
-    humans = []
-    for box_ind in boxes:
-        each = Human(boxes[box_ind]['id'], i, boxes[box_ind]['pose_pos'])
-        # print(boxes[box_ind]['pose_pos'])
-        humans.append(each)
-        # print(each)
-    # print(frame.__dict__)
-    frames.append(Frame(i, humans))
-    # print(len(humans))
-    if (hum_cnt < len(boxes)):
-        hum_cnt = len(boxes)
+# for i in range(len(json_data)):
+#     boxes = json_data[str(i)]
+#     # if (len(boxes) == 0):
+#     #     continue
+#     humans = []
+#     for box_ind in boxes:
+#         each = Human(boxes[box_ind]['id'], i, boxes[box_ind]['pose_pos'])
+#         # print(boxes[box_ind]['pose_pos'])
+#         humans.append(each)
+#         # print(each)
+#     # print(frame.__dict__)
+#     frames.append(Frame(i, humans))
+#     # print(len(humans))
+#     if (hum_cnt < len(boxes)):
+#         hum_cnt = len(boxes)
 
 # Video(frames, behaviors, hum_cnt)
 # print("number of humans in this video: ", hum_cnt)
@@ -105,20 +172,20 @@ behaviors.append(Behavior(0, [9,10]))
 #     # print(anker)
 #     human.pose_pos[anker]
 
-def Track(bh, start, end):
-    trk = []
-    for frame_id in range(start, end):
-        for human in Frame.get(frame_id).humans:
-            li = [ human.pose_pos[anker] for anker in Behavior.get(bh).akpnt]
-            trk.append(json.dumps({'frame_id': human.frame_id, 'human_id': human.id, 'pose_pos': li}, sort_keys=True))
-    return trk
+# def Track(bh, start, end):
+#     trk = []
+#     for frame_id in range(start, end):
+#         for human in Frame.get(frame_id).humans:
+#             li = [ human.pose_pos[anker] for anker in Behavior.get(bh).akpnt]
+#             trk.append(json.dumps({'frame_id': human.frame_id, 'human_id': human.id, 'pose_pos': li}, sort_keys=True))
+#     return trk
 
 
-print(Track(0, 9, 11))
+# print(Track(0, 9, 11))
 
-def getAnkpntInBehavior(idx):
-    return Behavior.get(idx).akpnt
+# def getAnkpntInBehavior(idx):
+#     return Behavior.get(idx).akpnt
 
-print (getAnkpntInBehavior(0))
+# print (getAnkpntInBehavior(0))
 
 
---
+++ test.py
@@ -0,0 +1,24 @@
+from parse import Behavior
+from parse import Human
+from parse import Frame
+from parse import Video
+
+""" initialize behavior """
+clap = Behavior(0, [9,10])
+raiseArm = Behavior(1, [0,1,4,7])
+
+print (clap.akpnt)
+
+""" initialize video """
+video = Video('../(G)I-DLE-01_matching.json')
+
+print ("There are ", video.hmcnt, "people in this video")
+print ("There are ", len(video.frame.get(9).humans), "people in this frame")
+print ("There are ", video.frame.getLength(), "frames")
+
+""" track frame 99 ~ 101 by clap behavior """
+print(video.Track(clap, 99, 101))
+
+
+
+
