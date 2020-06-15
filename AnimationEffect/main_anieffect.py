4b6691818e989ff154ae04a2c5996fc641467679
jungeon-park <wnddjs97@naver.com>
2020-06-15 23:16:22 +0900

    main

--- AnimationEffect/main_anieffect.py
+++ AnimationEffect/main_anieffect.py
@@ -21,87 +21,87 @@ from modules.back_light4 import back_light4_effect
 from modules.color_outline import outline_effect
 from modules.color_outline_black import black_outline_effect
 
-# in_video_path = '../../Naver_video_02.mp4'
-# out_video_path = '../../proto_01.mp4'
+in_video_path = '../../Naver_video_02.mp4'
+out_video_path = '../../proto_01.mp4'
 
-# cap = cv2.VideoCapture(in_video_path)
-# back_cap = cv2.VideoCapture(in_video_path)
+cap = cv2.VideoCapture(in_video_path)
+back_cap = cv2.VideoCapture(in_video_path)
 
-# width = int(cap.get(3))
-# height = int(cap.get(4))
-# fps = int(cap.get(cv2.CAP_PROP_FPS))
+width = int(cap.get(3))
+height = int(cap.get(4))
+fps = int(cap.get(cv2.CAP_PROP_FPS))
 
-# fourcc = cv2.VideoWriter_fourcc(*'mp4v')
-# out = cv2.VideoWriter(out_video_path, fourcc, fps, (width, height))
+fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+out = cv2.VideoWriter(out_video_path, fourcc, fps, (width, height))
 
-# # video effect start
-# i = 0
-# while(cap.isOpened()):
-#     ret, frame = cap.read()
-#     back_ret, back_frame = back_cap.read() # original frame / It's for opacity
+# video effect start
+i = 0
+while(cap.isOpened()):
+    ret, frame = cap.read()
+    back_ret, back_frame = back_cap.read() # original frame / It's for opacity
 
-#     if ret == False:
-#         print("Oops... ")
-#         break
+    if ret == False:
+        print("Oops... ")
+        break
 
-#     #Skip the unrecognized frame
-#     if in_video.frames[i] == 'empty_frame':
-#         i += 1
-#         continue
+    #Skip the unrecognized frame
+    if in_video.frames[i] == 'empty_frame':
+        i += 1
+        continue
 
-#     # Short Test
-#     # if i > 2400 :
-#     #     break
+    # Short Test
+    # if i > 2400 :
+    #     break
 
-#     # effect modules
-#     if i == 30:
-#         i, frame, back_frame = back_streak_effect(cap,frame, back_cap,back_frame, out, in_video, i)
+    # effect modules
+    if i == 30:
+        i, frame, back_frame = back_streak_effect(cap,frame, back_cap,back_frame, out, in_video, i)
 
-#     if i == 200:
-#         i, frame, back_frame = ribbon_effect(cap,frame, back_cap,back_frame, out, in_video, i)
+    if i == 200:
+        i, frame, back_frame = ribbon_effect(cap,frame, back_cap,back_frame, out, in_video, i)
 
-#     if i == 400:
-#         i, frame, back_frame = heart1_effect(cap,frame, back_cap,back_frame, out, in_video, i)
+    if i == 400:
+        i, frame, back_frame = heart1_effect(cap,frame, back_cap,back_frame, out, in_video, i)
     
-#     if i == 500:
-#         i, frame, back_frame = back_turnnel_effect(cap,frame, back_cap,back_frame, out, in_video, i)
+    if i == 500:
+        i, frame, back_frame = back_turnnel_effect(cap,frame, back_cap,back_frame, out, in_video, i)
 
-#     if i == 610:
-#         i, frame, back_frame = back_light3_effect(cap,frame, back_cap,back_frame, out, in_video, i)
+    if i == 610:
+        i, frame, back_frame = back_light3_effect(cap,frame, back_cap,back_frame, out, in_video, i)
     
-#     if i == 800:
-#         i, frame, back_frame = heart2_effects(cap,frame, back_cap,back_frame, out, in_video, i)
+    if i == 800:
+        i, frame, back_frame = heart2_effects(cap,frame, back_cap,back_frame, out, in_video, i)
 
-#     if i == 900:
-#         i, frame, back_frame = fire_effect(cap,frame, back_cap,back_frame, out, in_video, i, 80)
+    if i == 900:
+        i, frame, back_frame = fire_effect(cap,frame, back_cap,back_frame, out, in_video, i, 80)
 
-#     if i == 1000:
-#         i, frame, back_frame = handneck_effect(cap,frame, back_cap,back_frame, out, in_video, i, 100)
+    if i == 1000:
+        i, frame, back_frame = handneck_effect(cap,frame, back_cap,back_frame, out, in_video, i, 100)
   
-#     if i == 1150:
-#         i, frame, back_frame = wing_effect(cap,frame, back_cap,back_frame, out, in_video, i)
+    if i == 1150:
+        i, frame, back_frame = wing_effect(cap,frame, back_cap,back_frame, out, in_video, i)
     
-#     if i == 1300:
-#         i, frame, back_frame = outline_effect(cap,frame, back_cap,back_frame, out, in_video, i)
+    if i == 1300:
+        i, frame, back_frame = outline_effect(cap,frame, back_cap,back_frame, out, in_video, i)
     
-#     if i == 1450:
-#         i, frame, back_frame = back_glowing_effect(cap,frame, back_cap,back_frame, out, in_video, i)
+    if i == 1450:
+        i, frame, back_frame = back_glowing_effect(cap,frame, back_cap,back_frame, out, in_video, i)
     
-#     if i == 1750:
-#         i, frame, back_frame = back_light2_effect(cap,frame, back_cap,back_frame, out, in_video, i)
+    if i == 1750:
+        i, frame, back_frame = back_light2_effect(cap,frame, back_cap,back_frame, out, in_video, i)
 
-#     if i == 2180:
-#         i, frame, back_frame = black_outline_effect(cap,frame, back_cap,back_frame, out, in_video, i)
-#     # write output frame
-#     out.write(frame)
+    if i == 2180:
+        i, frame, back_frame = black_outline_effect(cap,frame, back_cap,back_frame, out, in_video, i)
+    # write output frame
+    out.write(frame)
 
-#     i += 1
+    i += 1
 
-# cap.release()
-# out.release()
-# cv2.destroyAllWindows()
+cap.release()
+out.release()
+cv2.destroyAllWindows()
 
-# print("Done!")
+print("Done!")
 
 # add audio
 my_clip = mpe.VideoFileClip('../../proto_01.mp4')
